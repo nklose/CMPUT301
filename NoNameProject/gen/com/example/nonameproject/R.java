a2e3a131a5f5342be03a2f68e13d46ed2dbf5951
Nick Klose <nick.klose@ualberta.ca>
2012-11-04 16:10:21 -0700

    updated main and add_task activities; added completed_tasks

--- NoNameProject/gen/com/example/nonameproject/R.java
+++ NoNameProject/gen/com/example/nonameproject/R.java
@@ -16,28 +16,34 @@ public final class R {
         public static final int ic_refresh=0x7f020002;
     }
     public static final class id {
-        public static final int addTaskRadioGroup=0x7f070005;
-        public static final int addTaskTextAudio=0x7f070008;
-        public static final int addTaskTextImage=0x7f070007;
-        public static final int addTaskTextRadio=0x7f070006;
-        public static final int addtaskbutton=0x7f07000d;
-        public static final int cancelTaskButton=0x7f07000b;
-        public static final int createTaskButton=0x7f07000c;
-        public static final int itemsRequested=0x7f070009;
-        public static final int menu_settings=0x7f070011;
-        public static final int refreshButton=0x7f070000;
-        public static final int shareOnlineCheckbox=0x7f07000a;
-        public static final int taskCreator=0x7f070003;
-        public static final int taskDescription=0x7f070002;
-        public static final int taskTitle=0x7f070001;
-        public static final int textView1=0x7f070004;
-        public static final int viewcompletedtasksbutton=0x7f070010;
-        public static final int viewlocaltasksbutton=0x7f07000e;
-        public static final int viewonlinetasksbutton=0x7f07000f;
+        public static final int addTaskRadioGroup=0x7f070004;
+        public static final int addTaskTextAudio=0x7f070007;
+        public static final int addTaskTextImage=0x7f070006;
+        public static final int addTaskTextRadio=0x7f070005;
+        public static final int addtaskbutton=0x7f070012;
+        public static final int allTasksRadio=0x7f070010;
+        public static final int cancelTaskButton=0x7f07000a;
+        public static final int completedTasksListView=0x7f070011;
+        public static final int completedTasksRadio=0x7f07000e;
+        public static final int createTaskButton=0x7f07000b;
+        public static final int itemsRequested=0x7f070008;
+        public static final int menu_settings=0x7f070016;
+        public static final int myTasksRadio=0x7f07000f;
+        public static final int refreshButton=0x7f07000d;
+        public static final int shareOnlineCheckbox=0x7f070009;
+        public static final int taskCreator=0x7f070002;
+        public static final int taskDescription=0x7f070001;
+        public static final int taskTitle=0x7f070000;
+        public static final int textView1=0x7f070003;
+        public static final int textView2=0x7f07000c;
+        public static final int viewcompletedtasksbutton=0x7f070015;
+        public static final int viewlocaltasksbutton=0x7f070013;
+        public static final int viewonlinetasksbutton=0x7f070014;
     }
     public static final class layout {
         public static final int activity_add_task=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int activity_completed_tasks=0x7f030001;
+        public static final int activity_main=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
@@ -52,10 +58,13 @@ public final class R {
         public static final int add_task_text_radio=0x7f04000c;
         public static final int add_task_title=0x7f040007;
         public static final int add_task_type=0x7f04000b;
+        public static final int all_tasks_label_radio=0x7f040014;
         public static final int app_name=0x7f040002;
         public static final int cancel_task_button=0x7f040011;
+        public static final int completed_tasks_title=0x7f040012;
         public static final int create_task_button=0x7f040010;
         public static final int menu_settings=0x7f040000;
+        public static final int my_tasks_label_radio=0x7f040013;
         public static final int share_online_checkbox=0x7f04000f;
         public static final int title_activity_main=0x7f040001;
         public static final int view_completed_tasks=0x7f040006;
