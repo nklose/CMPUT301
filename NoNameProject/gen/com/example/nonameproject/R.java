2e23dcf89af64f5411836b57c0df4cd7f28b6857
Nick Klose <nick.klose@ualberta.ca>
2012-11-04 16:33:37 -0700

    added activity_completed_tasks

--- NoNameProject/gen/com/example/nonameproject/R.java
+++ NoNameProject/gen/com/example/nonameproject/R.java
@@ -25,17 +25,24 @@ public final class R {
         public static final int cancelTaskButton=0x7f07000a;
         public static final int completedTasksListView=0x7f070011;
         public static final int completedTasksRadio=0x7f07000e;
+        public static final int completedTasksTitle=0x7f07000c;
         public static final int createTaskButton=0x7f07000b;
         public static final int itemsRequested=0x7f070008;
-        public static final int menu_settings=0x7f070016;
+        public static final int menu_settings=0x7f07001d;
         public static final int myTasksRadio=0x7f07000f;
         public static final int refreshButton=0x7f07000d;
         public static final int shareOnlineCheckbox=0x7f070009;
+        public static final int solutionTaskCreator=0x7f070019;
+        public static final int solutionTaskDesc=0x7f070018;
+        public static final int solutionTaskItemsRequested=0x7f07001b;
+        public static final int solutionTaskTitle=0x7f070017;
+        public static final int solutionTaskType=0x7f07001a;
         public static final int taskCreator=0x7f070002;
         public static final int taskDescription=0x7f070001;
+        public static final int taskSolutionItemsListView=0x7f07001c;
+        public static final int taskSolutionTitle=0x7f070016;
         public static final int taskTitle=0x7f070000;
         public static final int textView1=0x7f070003;
-        public static final int textView2=0x7f07000c;
         public static final int viewcompletedtasksbutton=0x7f070015;
         public static final int viewlocaltasksbutton=0x7f070013;
         public static final int viewonlinetasksbutton=0x7f070014;
@@ -44,6 +51,7 @@ public final class R {
         public static final int activity_add_task=0x7f030000;
         public static final int activity_completed_tasks=0x7f030001;
         public static final int activity_main=0x7f030002;
+        public static final int activity_task_solution=0x7f030003;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
@@ -66,6 +74,17 @@ public final class R {
         public static final int menu_settings=0x7f040000;
         public static final int my_tasks_label_radio=0x7f040013;
         public static final int share_online_checkbox=0x7f04000f;
+        public static final int task_creator_label=0x7f040018;
+        public static final int task_creator_placeholder=0x7f04001d;
+        public static final int task_desc_label=0x7f040017;
+        public static final int task_desc_placeholder=0x7f04001c;
+        public static final int task_items_requested_label=0x7f04001a;
+        public static final int task_items_requested_placeholder=0x7f04001f;
+        public static final int task_solution_title=0x7f040015;
+        public static final int task_title_label=0x7f040016;
+        public static final int task_title_placeholder=0x7f04001b;
+        public static final int task_type_label=0x7f040019;
+        public static final int task_type_placeholder=0x7f04001e;
         public static final int title_activity_main=0x7f040001;
         public static final int view_completed_tasks=0x7f040006;
         public static final int view_local_tasks=0x7f040004;
