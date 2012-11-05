a8a1683be7fd3ff3cf7eb8ed675f42ce4165a3cf
Nick Klose <nick.klose@ualberta.ca>
2012-11-04 17:29:17 -0700

    finished all layouts except shared_tasks_list and view_shared_task

--- NoNameProject/gen/com/example/nonameproject/R.java
+++ NoNameProject/gen/com/example/nonameproject/R.java
@@ -16,11 +16,12 @@ public final class R {
         public static final int ic_refresh=0x7f020002;
     }
     public static final class id {
+        public static final int addItemLocalTaskButton=0x7f070014;
         public static final int addTaskRadioGroup=0x7f070004;
         public static final int addTaskTextAudio=0x7f070007;
         public static final int addTaskTextImage=0x7f070006;
         public static final int addTaskTextRadio=0x7f070005;
-        public static final int addtaskbutton=0x7f070012;
+        public static final int addtaskbutton=0x7f070018;
         public static final int allTasksRadio=0x7f070010;
         public static final int cancelTaskButton=0x7f07000a;
         public static final int completedTasksListView=0x7f070011;
@@ -28,35 +29,43 @@ public final class R {
         public static final int completedTasksTitle=0x7f07000c;
         public static final int createTaskButton=0x7f07000b;
         public static final int itemsRequested=0x7f070008;
-        public static final int menu_settings=0x7f07001d;
+        public static final int localTaskItemsListView=0x7f070015;
+        public static final int localTasksListTitle=0x7f070016;
+        public static final int localTasksListView=0x7f070017;
+        public static final int menu_settings=0x7f070023;
         public static final int myTasksRadio=0x7f07000f;
         public static final int refreshButton=0x7f07000d;
+        public static final int saveChangesLocalTaskButton=0x7f070013;
+        public static final int shareLocalTaskButton=0x7f070012;
         public static final int shareOnlineCheckbox=0x7f070009;
-        public static final int solutionTaskCreator=0x7f070019;
-        public static final int solutionTaskDesc=0x7f070018;
-        public static final int solutionTaskItemsRequested=0x7f07001b;
-        public static final int solutionTaskTitle=0x7f070017;
-        public static final int solutionTaskType=0x7f07001a;
+        public static final int solutionTaskCreator=0x7f07001f;
+        public static final int solutionTaskDesc=0x7f07001e;
+        public static final int solutionTaskItemsRequested=0x7f070021;
+        public static final int solutionTaskTitle=0x7f07001d;
+        public static final int solutionTaskType=0x7f070020;
         public static final int taskCreator=0x7f070002;
         public static final int taskDescription=0x7f070001;
-        public static final int taskSolutionItemsListView=0x7f07001c;
-        public static final int taskSolutionTitle=0x7f070016;
+        public static final int taskSolutionItemsListView=0x7f070022;
+        public static final int taskSolutionTitle=0x7f07001c;
         public static final int taskTitle=0x7f070000;
         public static final int textView1=0x7f070003;
-        public static final int viewcompletedtasksbutton=0x7f070015;
-        public static final int viewlocaltasksbutton=0x7f070013;
-        public static final int viewonlinetasksbutton=0x7f070014;
+        public static final int viewcompletedtasksbutton=0x7f07001b;
+        public static final int viewlocaltasksbutton=0x7f070019;
+        public static final int viewonlinetasksbutton=0x7f07001a;
     }
     public static final class layout {
         public static final int activity_add_task=0x7f030000;
         public static final int activity_completed_tasks=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int activity_task_solution=0x7f030003;
+        public static final int activity_edit_local_task=0x7f030002;
+        public static final int activity_local_tasks_list=0x7f030003;
+        public static final int activity_main=0x7f030004;
+        public static final int activity_task_solution=0x7f030005;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
+        public static final int add_item_local_task_button=0x7f040021;
         public static final int add_task=0x7f040003;
         public static final int add_task_audio_radio=0x7f04000e;
         public static final int add_task_creator=0x7f040009;
@@ -71,8 +80,11 @@ public final class R {
         public static final int cancel_task_button=0x7f040011;
         public static final int completed_tasks_title=0x7f040012;
         public static final int create_task_button=0x7f040010;
+        public static final int local_tasks_list_title=0x7f040020;
         public static final int menu_settings=0x7f040000;
         public static final int my_tasks_label_radio=0x7f040013;
+        public static final int save_changes_button=0x7f040023;
+        public static final int share_button=0x7f040022;
         public static final int share_online_checkbox=0x7f04000f;
         public static final int task_creator_label=0x7f040018;
         public static final int task_creator_placeholder=0x7f04001d;
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 CMPUT301
 ========
+
+Team: CMPUT301F12T10 (cbeckett, klose, teschnei)
+
+License
+=======
+This application allows the user to input log entries with information
+about their laptop's current battery state. The purpose is to see how
+much battery life is lost while using the laptop for various tasks.
+
+Copyright (C) 2012 Christopher Beckett, Nicholas Klose, Tyler Schneider
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+http://www.gnu.org/copyleft/gpl.html
