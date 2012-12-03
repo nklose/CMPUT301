b998a0e2ad1780027b4032e4b5fe5a4269162d62
Nick Klose <nick.klose@ualberta.ca>
2012-12-03 15:31:59 -0700

    added javadoc comments to SharedTaskController

--- NoNameProject/src/com/example/nonameproject/SharedTaskController.java
+++ NoNameProject/src/com/example/nonameproject/SharedTaskController.java
@@ -1,26 +1,19 @@
 package com.example.nonameproject;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import android.app.Activity;
+
+import android.content.Context;
 
 import com.example.nonameproject.Activities.ViewSharedTasksActivity;
 import com.example.nonameproject.network.AddSharedTaskThread;
 import com.example.nonameproject.network.DeleteSharedTaskThread;
 import com.example.nonameproject.network.GetSharedTasksThread;
 
-
-import android.content.Context;
-
 /**
- * Controller responsible for modifying and reading shared tasks used by the application
+ * Controller responsible for modifying and reading shared tasks used by the application.
  */
 public class SharedTaskController extends TaskController{
 
-
-	/**
-	 * 
-	 */
 	public SharedTaskController(Context context) {
 		super();
 		if( this.tasks.size() == 0){
@@ -28,22 +21,37 @@ public class SharedTaskController extends TaskController{
 		}
 	}
 
+	/**
+	 * Sets the list of tasks to the given arraylist.
+	 * @param tasks ArrayList of Task objects.
+	 */
 	public void addAllTasks(ArrayList<Task> tasks){
 		this.tasks = tasks;
 	}
 	
+	/**
+	 * Adds a task to the list of tasks.
+	 * @param context Current context.
+	 * @param newTask Task object to be added.
+	 */
 	public void addTask(Context context, Task newTask) {
 		tasks.add(newTask);
 		new AddSharedTaskThread().execute(newTask);
 	}
 
+	/**
+	 * Removes a given task from the list of tasks.
+	 * @param context Current context.
+	 * @param task Task to be removed.
+	 */
 	public void deleteTask(Context context, Task task) {
 		tasks.remove(task);
 		new DeleteSharedTaskThread().execute(task);
 	}
 
 	/**
-	 * @param fis
+	 * Reads the file containing the list of shared tasks.
+	 * @param context Current context.
 	 */
 	public void readSharedTaskFile(Context context){
 		if( context instanceof ViewSharedTasksActivity){
@@ -52,12 +60,10 @@ public class SharedTaskController extends TaskController{
 		new GetSharedTasksThread().execute(this, context);
 	}
 	
-	/*
-	 * Deletes the shared tasks both in memory and crowdsourcer
-	 * Used in the teardown of tests
-	 * 
-	 * @param
-	 * 	context - 
+	/**
+	 * Deletes the shared tasks both in memory and in the CrowdSourcer.
+	 * Used in the tear-down of tests.
+	 * @param context Current context. 
 	 */
 	public void nuke(){
 		tasks = new ArrayList<Task>();
