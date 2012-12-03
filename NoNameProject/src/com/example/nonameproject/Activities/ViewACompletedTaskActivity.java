dd12b267e5b36db3c672d497b956abdb1d7fe465
cbeckett <cbeckett@ualberta.ca>
2012-12-03 02:07:00 -0700

    Finished adding long click completed task deletion

--- NoNameProject/src/com/example/nonameproject/Activities/ViewACompletedTaskActivity.java
+++ NoNameProject/src/com/example/nonameproject/Activities/ViewACompletedTaskActivity.java
@@ -1,69 +1,45 @@
 package com.example.nonameproject.Activities;
 
-import java.util.Calendar;
-
 import com.example.nonameproject.CompletedTaskController;
 import com.example.nonameproject.NoNameApp;
 import com.example.nonameproject.R;
-import com.example.nonameproject.SharedTaskController;
 import com.example.nonameproject.Task;
-import com.example.nonameproject.TextItem;
-import com.example.nonameproject.R.layout;
-import com.example.nonameproject.R.menu;
+
 
 import android.os.Bundle;
-import android.provider.Settings.Secure;
 import android.app.Activity;
-import android.app.Dialog;
-import android.content.Context;
 import android.content.Intent;
-import android.view.Menu;
-import android.view.View;
-import android.view.Window;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class ViewACompletedTaskActivity extends Activity {
 	private int position;	
-	private CompletedTaskController controller = NoNameApp.getCompletedTaskController(this);
-	private Dialog dialog;
+	private CompletedTaskController controller = NoNameApp.getCompletedTaskController();
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_view_acompleted_task);
-		Intent intent = getIntent();
-		String deviceId = Secure.getString(this.getContentResolver(), Secure.ANDROID_ID); 
+		Intent intent = getIntent(); 
 		position = intent.getIntExtra("position", 0);
 		Task task = controller.getTask(position);
 
 		TextView textView = (TextView) findViewById(R.id.taskTitle);
 		textView.setText(task.getTitle());
-		if( deviceId.equals(task.getDeviceId())){
-			textView.setEnabled(true);
-			textView.setFocusable(true);
-		}
+
 		textView = (TextView) findViewById(R.id.taskDescription);
 		textView.setText(task.getDescription());
-		if( deviceId.equals(task.getDeviceId())){
-			textView.setEnabled(true);
-			textView.setFocusable(true);
-		}
+
 		textView = (TextView) findViewById(R.id.taskCreator);
 		textView.setText(task.getCreator());
-		if( deviceId.equals(task.getDeviceId())){
-			textView.setEnabled(true);
-			textView.setFocusable(true);
-		}
 
 		Task.TaskType type = task.getType();	
 		RadioButton textRB = (RadioButton) findViewById(R.id.addTaskTextRadio);
+		textRB.setFocusable(false);
 		RadioButton imageRB = (RadioButton) findViewById(R.id.addTaskImageRadio);
+		imageRB.setFocusable(false);
 		RadioButton audioRB = (RadioButton) findViewById(R.id.addTaskAudioRadio);
+		audioRB.setFocusable(false);
 		if (type == Task.TaskType.TASK_TEXT){
 			textRB.setChecked(true);
 		} else if (type == Task.TaskType.TASK_IMAGE){
--- NoNameProject/src/com/example/nonameproject/Activities/ViewCompletedTasksActivity.java
+++ NoNameProject/src/com/example/nonameproject/Activities/ViewCompletedTasksActivity.java
@@ -1,19 +1,30 @@
 package com.example.nonameproject.Activities;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings.Secure;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.nonameproject.CompletedTaskBaseAdapter;
+import com.example.nonameproject.CompletedTaskController;
+import com.example.nonameproject.LocalTaskController;
+import com.example.nonameproject.Task;
+import com.example.nonameproject.NoNameApp;
 import com.example.nonameproject.R;
+
+
 
 public class ViewCompletedTasksActivity extends Activity {
 	private CompletedTaskBaseAdapter adapter = new CompletedTaskBaseAdapter(this);
+	private static String deviceId;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,6 +33,8 @@ public class ViewCompletedTasksActivity extends Activity {
 		final ListView listViewLog = (ListView) findViewById(R.id.localTasksListView);
 
 		try{
+			deviceId = Secure.getString(this.getContentResolver(), Secure.ANDROID_ID);
+
 			adapter = new CompletedTaskBaseAdapter(this);
 			listViewLog.setAdapter(adapter);
 			listViewLog.setOnItemClickListener(new OnItemClickListener() {
@@ -30,6 +43,53 @@ public class ViewCompletedTasksActivity extends Activity {
 					Intent intent = new Intent(ViewCompletedTasksActivity.this, ViewTaskSolutionActivity.class);
 					intent.putExtra("position", position);
 					startActivity(intent);
+				}
+			});
+
+			// listen for item long presses
+			listViewLog.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+				public boolean onItemLongClick(AdapterView<?> a, View v, final int position, long id) 
+				{	
+					CompletedTaskController controller = NoNameApp.getCompletedTaskController();
+					Task task = controller.getTask(position);
+					if( deviceId.equals(task.getDeviceId())){
+						// create a dialog to confirm deletion
+						AlertDialog.Builder alertDialog = new AlertDialog.Builder(ViewCompletedTasksActivity.this);
+						alertDialog.setTitle("Delete this task?");
+						//alertDialog.setMessage("Are you sure you want delete this?");
+
+						// yes button
+						alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int which) {
+								which = position;
+
+								// get controller 
+								CompletedTaskController controller = NoNameApp.getCompletedTaskController();
+
+								// delete the task
+								controller.deleteTask(getBaseContext(), position);
+
+								// restart the activity
+								finish();
+								startActivity(getIntent());
+							}
+						});
+
+						alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() 
+						{
+							public void onClick(DialogInterface dialog, int which) 
+							{
+								dialog.cancel();
+							}
+						});
+
+						// show delete confirmation dialog
+						alertDialog.show();
+
+						return true;
+					}
+					return true;
 				}
 			});
 		}
